1f768b6654724d9b1e7418319a44eba6fc1ee9ac
Dotnet-And-Happiness <nazarnik050406@gmail.com>
2021-05-08 10:49:07 +0300

    Added Convertor app sample

---
+++ samples/Converter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Converter
+{
+    [App]
+    public class Program
+    {
+        public class To 
+        {
+            public static void Bin(long number)
+            {
+                Console.WriteLine(Convert.ToString(number, 2));
+            }
+            public static void Octo(long number)
+            {
+                Console.WriteLine(Convert.ToString(number, 8));
+            }
+            public static void Hex(long number)
+            {
+                Console.WriteLine(Convert.ToString(number, 16));
+            }
+        }
+        public class From 
+        {
+            public static void Bin(string number)
+            {
+                Console.WriteLine(Convert.ToInt64(number, 2));
+            }
+            public static void Octo(string number)
+            {
+                Console.WriteLine(Convert.ToInt64(number, 8));
+            }
+            public static void Hex(string number)
+            {
+                Console.WriteLine(Convert.ToInt64(number, 16));
+            }
+        }
+    }
+}
