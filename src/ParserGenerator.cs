c888d2a187301566e9f7922060eece7ab5eb29c3
Dotnet-And-Happiness <nazarnik050406@gmail.com>
2021-05-09 14:05:37 +0300

    Returning to Scriban

--- src/ParserGenerator.cs
+++ src/ParserGenerator.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.IO;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using Scriban;
+using Scriban.Runtime;
 using System.Xml.Linq;
 using Microsoft.CodeAnalysis.CSharp;
 using System.Diagnostics;
@@ -16,6 +19,14 @@ namespace CommandLineArgsGenerator
     public class ParserGenerator : ISourceGenerator
     {
         private static SymbolDisplayFormat typeFormat = new SymbolDisplayFormat(typeQualificationStyle: SymbolDisplayTypeQualificationStyle.NameAndContainingTypesAndNamespaces);
+        private static readonly Template template;
+        static ParserGenerator()
+        {
+            var asm = Assembly.GetExecutingAssembly();
+            using var stream = asm.GetManifestResourceStream(asm.GetManifestResourceNames().FirstOrDefault(x => x.EndsWith(".template")));
+            using var reader = new System.IO.StreamReader(stream);
+            template = Template.Parse(reader.ReadToEnd());
+        }
         public void Execute(GeneratorExecutionContext context)
         { 
             var receiver = context.SyntaxContextReceiver as ParserSyntaxReceiver; 
@@ -25,13 +36,23 @@ namespace CommandLineArgsGenerator
                 var cmds = GetCommands(receiver.Root.Class, semanticModel, out ICommandInfo defaultCommand);
                 receiver.Root.Children = cmds;
                 receiver.Root.Default = defaultCommand;
-                var t = new
+                var model = new
                     {
                         Namespace = receiver.Namespace.Trim(),
                         Root = receiver.Root,
                         Converters = receiver.Converters.GroupBy(x => x.Value).ToDictionary(t => t.Key, t => t.Select(r => r.Key).ToList())
-                    };  
-                context.AddSource("EntryPoint.cs",  Template.RenderAsync(t).Result);
+                    }; 
+                var sc = new ScriptObject();  
+                
+                sc.Import("HasMethod", (Func<ParameterInfo, string, int, bool>)((param,name,args) => param.HasMethod(name, args)));
+                sc.Import("HasCtor", (Func<ParameterInfo, IEnumerable<object>, bool>)((param, args) => param.HasCtor(args.Cast<string>().ToArray())));
+                
+                var ctx = new TemplateContext();
+                ctx.PushGlobal(sc);
+                sc.Import(model, x => true , x => x.Name);
+                ctx.MemberRenamer = x => x.Name; 
+                
+                context.AddSource("EntryPoint.cs", template.Render(ctx));
 
             }
 
