090c11279ca5b7cae911d137fa10327e4b26960d
Dotnet-And-Happiness <nazarnik050406@gmail.com>
2021-05-26 16:24:06 +0300

    Boolean better parsing

--- src/OptionInfo.cs
+++ src/OptionInfo.cs
@@ -1,9 +1,11 @@
-﻿namespace CommandLineArgsGenerator
+﻿using Microsoft.CodeAnalysis;
+namespace CommandLineArgsGenerator
 {
     public class OptionInfo : ParameterInfo
     {
         public string? Default { get; set; }
         public bool IsArray { get; set; }
+        public bool IsBool => Type?.SpecialType == SpecialType.System_Boolean;
     }
 
 } 
