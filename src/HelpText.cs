fe7e58444ab10172f4e194182f45133f2556d6de
xb-bx <nazarnik050406@gmail.com>
2021-08-25 11:19:03 +0300

    Added method for constructing HelpText from XElement

--- src/HelpText.cs
+++ src/HelpText.cs
@@ -1,4 +1,7 @@
 using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Linq;
+using System;
 namespace CommandLineArgsGenerator
 {
     public struct HelpText
@@ -20,6 +23,23 @@ namespace CommandLineArgsGenerator
 				else
 					texts.Add(culture, value!);
 			}
+		}
+		public static HelpText FromXElement(XElement elem, string defaultCulture) 
+		{
+			HelpText help = new();
+			foreach(var node in elem.Nodes())
+			{
+				if(node.NodeType == XmlNodeType.Text) 
+				{
+					help[defaultCulture] = (node as XText).Value.Trim();
+				}
+				else
+				{
+					var el = node as XElement;
+					help[el.Name.ToString()] = el.Value.Trim();
+				}
+			}
+			return help;
 		}
 	}
 } 
